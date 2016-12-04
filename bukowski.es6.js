2a1d284df435c7eea8a4975d9d6d7570f71636be
s tech <email@sarcas.tech>
2016-12-04 17:04:44 -0500

    breaking out module for es6 & commonJS formats

---
+++ bukowski.es6.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let defaults = {
+  speed: 50,
+  element: null,
+  text: '',
+  counter: 0,
+  cursor: '_'
+}
+
+let Bukowski = function (config) {
+  this.config = Object.assign(defaults, config)
+  this.interval = setInterval(this.play(), this.config.speed)
+}
+
+Bukowski.prototype = {
+  play: function () {
+    this.element.innerHTML = this.text.substr(0, this.counter) + this.cursor
+
+    if (this.counter <= this.text.length) {
+      this.counter += 1
+    } else {
+      this.counter = 0
+      this.element.innerHTML = this.text
+      clearInterval(this.interval)
+    }
+  }
+}
+
+export default Bukowski
